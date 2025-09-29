222457d15b0b45e357629bf3305e040b6be57f44
Santiago Aramburu <santiburu77@gmail.com>
2025-09-29 14:59:40 -0400

    Update Hoot Branch

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -14,7 +14,6 @@ router.post("/", verifyToken, async (req, res) => {
     const hoot = await Hoot.create(req.body);
     hoot._doc.author = req.user;
     res.status(201).json(hoot);
-
   } catch (err) {
     res.status(500).json({ err: err.message });
   }
@@ -31,17 +30,27 @@ router.get("/", verifyToken, async (req, res) => {
   }
 });
 
+//Update
+router.put("/:hootId", verifyToken, async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
 
+    if (!hoot.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
 
+    const updatedHoot = await Hoot.findByIdAndUpdate(
+      req.params.hootId,
+      req.body,
+      { new: true }
+    );
 
+    updatedHoot._doc.author = req.user;
 
-
-
-
-
-
-
-
-
+    res.status(200).json(updatedHoot);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
 
 module.exports = router;
