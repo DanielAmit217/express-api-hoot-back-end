3ce835c93bd0cad2413ef41d94e3d3915d2d11dd
Santiago Aramburu <santiburu77@gmail.com>
2025-09-29 13:51:24 -0400

    Index Branch

---
+++ controllers/hoots.js
@@ -0,0 +1,21 @@
+// controllers/hoots.js
+
+const express = require("express");
+const verifyToken = require("../middleware/verify-token.js");
+const Hoot = require("../models/hoot.js");
+const router = express.Router();
+
+// add routes here
+
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const hoots = await Hoot.find({})
+      .populate("author")
+      .sort({ createdAt: "desc" });
+    res.status(200).json(hoots);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+module.exports = router;
